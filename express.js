a200b01515b1d0f755dfd849329a69e917a0fba9
Logan Lentz <logan.lentz1@gmail.com>
2023-01-12 02:48:17 -0600

    cors allow

--- express.js
+++ express.js
@@ -9,6 +9,14 @@ const client = new Client({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+
 app.get('/cid-requests', async (req, res) => {
     try {
         const { body } = await client.search({
