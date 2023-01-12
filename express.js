6d59dbd83c9fbc51d529abf67de0ab2aad1fb23e
Logan Lentz <logan.lentz1@gmail.com>
2023-01-12 02:17:54 -0600

    express catch block for errors

--- express.js
+++ express.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const { Client } = require('@elastic/elasticsearch')
-const app = express()
-const port = 3000
+const express = require('express');
+const { Client } = require('@elastic/elasticsearch');
+const app = express();
 
-// create an Elasticsearch client
 const client = new Client({
     node: 'http://localhost:9200'
-})
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/cid-requests', async (req, res) => {
-    // search for CID requests in Elasticsearch
-    const { body } = await client.search({
-        index: 'cloudflare_worker_logs',
-        body: {
-            query: {
-                match: {
-                    "ipfs_hash": "CID_HASH"
+    try {
+        const { body } = await client.search({
+            index: 'cloudflare_worker_logs',
+            body: {
+                query: {
+                    match: {
+                        "ipfs_hash": req.query.hash
+                    }
                 }
             }
-        }
-    })
+        });
+        res.status(200).json({
+            cidRequests: body.hits.total.value
+        });
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
 
-    // return the number of CID requests in the response
-    res.json({
-        cidRequests: body.hits.total.value
-    })
-})
-
-app.listen(port, () => {
-    console.log(`API listening at http://localhost:${port}`)
-})
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
